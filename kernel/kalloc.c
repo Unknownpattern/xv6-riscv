9842f435838d723129f93fde6736d8c67f934aaa
Fangsheng Chao <fchao002@ucr.edu>
2021-02-10 17:05:56 -0800

    added refcount

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -17,18 +17,32 @@ extern char end[]; // first address after kernel.
 struct run {
   struct run *next;
 };
-
+static struct spinlock cowlock;
 struct {
   struct spinlock lock;
   struct run *freelist;
+  uint refCount[(PHYSTOP - KERNBASE ) >> PGSHIFT];
 } kmem;
-
+uint addr2index(void* r){
+  return ((uint)PGROUNDUP((uint64)r)-KERNBASE) >> PGSHIFT;
+}
+void refCountInit(){
+  acquire(&kmem.lock);
+  for(uint i = 0; i < ((PHYSTOP - KERNBASE ) >> PGSHIFT); i++){
+    //so that freerange will set them to 0... maybe
+    kmem.refCount[i] = 1;
+  }
+  release(&kmem.lock);
+}
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&cowlock, "cowlock");
+  refCountInit();
   freerange(end, (void*)PHYSTOP);
 }
+
 
 void
 freerange(void *pa_start, void *pa_end)
@@ -56,10 +70,22 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&cowlock);
+  if(kmem.refCount[addr2index(r)] > 1){
+    printf("decre index counter \n");
+    kmem.refCount[addr2index(r)] -= 1;
+  }
+  else{
+    kmem.refCount[addr2index(r)] -= 1;
+    if(kmem.refCount[addr2index(r)] != 0){
+      printf("Error, reference counter is : %d\n", kmem.refCount[addr2index(r)]);
+      panic("negative ref counter in kfree");
+    }
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+  }
+  
+  release(&cowlock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -72,11 +98,62 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r){
     kmem.freelist = r->next;
+    // add reference count whenever allocated
+  }
   release(&kmem.lock);
 
+  acquire(&cowlock);
+  if(r){
+    if(kmem.refCount[addr2index(r)] == 0)
+      kmem.refCount[addr2index(r)] += 1;
+    else{
+      printf("kalloc not an empty page?\n");
+    }
+  }
+  release(&cowlock);
+  
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+uint incref(void *pa){
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("incref out of bounds");
+  uint count;
+  struct run *r;
+  r = (struct run*)pa;
+  acquire(&cowlock);
+  if(kmem.refCount[addr2index(r)] != 0){
+     count = ++kmem.refCount[addr2index(r)];
+  }
+  else{
+    panic("incref on ref with 0?");
+  }
+  
+  release(&cowlock);
+  return count;
+}
+uint decref(void *pa){
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("decref out of bounds");
+  uint count;
+  struct run *r;
+  r = (struct run*) pa;
+  acquire(&cowlock);
+  if(kmem.refCount[addr2index(r)] != 0){
+     count = --kmem.refCount[addr2index(r)];
+  }
+  else{
+    panic("dec on ref with 0?");
+  }
+  release(&cowlock);
+  if(count == 0){
+    printf("ref 0, freeing\n");
+    kfree((char*)PGROUNDUP((uint64)pa));
+  }
+  
+  return count;
 }
